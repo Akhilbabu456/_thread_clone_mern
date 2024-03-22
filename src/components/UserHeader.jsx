35a930e335300e399956908cf81709312183f09c
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-03-23 00:44:31 +0530

    second commit

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,7 +1,22 @@
-import { Avatar, Box, Flex, Link, Text, VStack } from "@chakra-ui/react"
+import { Avatar, Box, Flex, Link, Menu, MenuButton, MenuItem, MenuList, Portal, Text, VStack, useToast } from "@chakra-ui/react"
 import {BsInstagram} from "react-icons/bs"
+import {CgMoreO} from "react-icons/cg"
 
 const UserHeader = () => {
+    const toast = useToast()
+
+    const copyUrl = ()=>{
+        const currentURL = window.location.href
+        navigator.clipboard.writeText(currentURL).then(()=>{
+            toast({
+                title: "Copied",
+                description: "Profile link copied.",
+                status: "success",
+                duration: 2000,
+                isClosable: true,
+                })
+        })
+    }
   return (
     <VStack gap={4} alignItems={"start"}>
       <Flex justifyContent={"space-between"} w={"full"}>
@@ -32,10 +47,31 @@ const UserHeader = () => {
             <Link color={"gray.light"}>instagram.com</Link>
           </Flex>
           <Flex>
-            <Box>
+            <Box className="icon-container">
                 <BsInstagram size={24} cursor={"pointer"}/>
             </Box>
+            <Box className="icon-container">
+                <Menu>
+                    <MenuButton>
+                      <CgMoreO size={24} cursor={"pointer"}/>
+                    </MenuButton>
+                    <Portal>
+                        <MenuList bg={"gray.dark"}>
+                            <MenuItem bg={"gray.dark"} onClick={copyUrl}>Copy Link</MenuItem>
+                        </MenuList>
+                    </Portal>
+                </Menu>
+            </Box>
           </Flex>
+      </Flex>
+
+      <Flex w={"full"}>
+        <Flex flex={1} borderBottom={"1.5px solid white"} justifyContent={"center"} pb={3} cursor={"pointer"}>
+          <Text fontWeight={"bold"}>Threads</Text>  
+        </Flex>
+        <Flex flex={1} borderBottom={"1px solid gray"} color={"gray.light"} justifyContent={"center"} pb={3} cursor={"pointer"}>
+          <Text fontWeight={"bold"}>Replies</Text>  
+        </Flex>
       </Flex>
     </VStack>
   )
