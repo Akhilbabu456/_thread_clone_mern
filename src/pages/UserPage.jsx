562c6ee03341b8d3fc9c2ecf0ce0f6084a2494d7
akhilbabu456 <akhilbeliever001@gmail.com>
2024-03-23 14:19:23 +0530

    third

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,10 +1,12 @@
 import UserHeader from "../components/UserHeader"
+import UserPost from "../components/UserPost"
 
 
 const UserPage = () => {
   return (
     <>
       <UserHeader/>
+      <UserPost/>
     </>
   )
 }
